5003132107257d3c6a33db245cc6bc02b24fe50d
Zene <database@zene.uk>
2025-09-16 21:21:50 +0100

    Get user folder

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use directories::BaseDirs;
+use colored::Colorize;
+
+macro_rules! print_error {
+    ($($arg:tt)*) => {{
+        let string = format!($($arg)*);
+        println!("{}", string.red());
+    }};
+}
+
 fn main() {
     println!("Hello, world!");
+    
+    if let Some(dir) = BaseDirs::new()
+    {
+        let folder = dir.data_dir().join("projup");
+        
+        
+    }
+    else
+    {
+        print_error!("Could not get user application folder.");
+    }
 }
